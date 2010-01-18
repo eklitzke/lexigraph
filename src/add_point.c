fafc480fb7f5bb82ca5b3273082803b76767cb0f
Evan Klitzke <evan@eklitzke.org>
2010-01-18 15:52:03 -0800

    get add_point working

--- src/add_point.c
+++ src/add_point.c
@@ -1,165 +1,105 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <curl/curl.h>
 
-int connected_socket(char *hostname, uint16_t port)
-{
-    int sock;
-    struct hostent *hostaddr;
-    struct sockaddr_in sa;
+#define LEXIGRAPH_DATAPOINT_URL "http://localhost:8080/api/new/datapoint"
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("socket()");
-        return -1;
-    }
-
-    memset(&sa, 0, sizeof(sa));
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(port);
-
-    hostaddr = gethostbyname(hostname);
-    if (!hostaddr) {
-        perror("gethostbyname()");
-        goto conn_err;
-    }
-    memcpy(&sa.sin_addr.s_addr, hostaddr->h_addr, hostaddr->h_length);
-
-    if (connect(sock, (struct sockaddr *) &sa, sizeof(sa)) < 0) {
-        perror("connect()");
-        goto conn_err;
-    }
-    return sock;
-
-conn_err:
-    close(sock);
-    return -1;
-}
-
-int sendall(int sock, char *buffer, size_t len)
-{
-    printf("%s", buffer);
-    return 0;
-#if 0
-    size_t offset;
-    ssize_t ret;
-
-    if (!len)
-        len = strlen(buffer);
-
-    offset = 0;
-    while (offset < len) {
-        ret = send(sock, buffer + offset, len - offset, 0);
-        if (ret < 0) {
-            perror("send");
-            return -1;
-        }
-        offset += ret;
-    }
-    return 0;
+#ifdef WITH_SSL
+#define LEXIGRAPH_CURL_OPTS CURL_GLOBAL_SSL
+#else
+#define LEXIGRAPH_CURL_OPTS 0
 #endif
+
+void print_usage()
+{
+    fprintf(stderr, "usage: add_point <api_key> <dataset> <value>\n");
 }
 
-/* *output will be malloced, and must be freed */
-ssize_t urlencode(char *input, size_t len, char **output)
-{
-    char c;
-    size_t i;
-    size_t offset = 0;
-    char *hex = "0123456789abcdef";
-
-    if (!len)
-        len = strlen(input);
-
-    /* worst case is a string 3x the length of the input */
-    *output = malloc(3 * len + 1);
-    if (*output < 0) {
-        perror("malloc()");
-        return -1;
+/* THIS WON'T WORK WITH SPACES */
+char * add_field(CURL *handle, char *postdata, char *field, char *unencoded) {
+    int ampersand;
+    char *encoded;
+    ampersand = strlen(postdata) == 0 ? 0 : 1;
+    if (ampersand) {
+        postdata = realloc(postdata, strlen(postdata) + strlen(field) + 2);
+    } else {
+        postdata = realloc(postdata, strlen(postdata) + strlen(field) + 1);
     }
-    for (i = 0; i < len; i++) {
-        c = input[i];
-        if ( ('a' <= c && c <= 'z')
-             || ('A' <= c && c <= 'Z')
-             || ('0' <= c && c <= '9')) {
-            (*output)[offset++] = c;
-        } else if (c == ' ') {
-            (*output)[offset++] = '+';
-        } else {
-            (*output)[offset++] = '%';
-            (*output)[offset++] = hex[c >> 4];
-            (*output)[offset++] = hex[c & 0xF];
-        }
+    if (postdata == NULL) {
+        perror("realloc()");
+        exit(EXIT_FAILURE);
     }
-    (*output)[offset] = '\0';
-    return offset ;
+    if (ampersand) {
+        strcat(postdata, "&");
+    }
+    strcat(postdata, field);
+    encoded = curl_easy_escape(handle, unencoded, 0);
+    if (encoded == NULL) {
+        perror("curl_easy_escape()");
+        exit(EXIT_FAILURE);
+    }
+    postdata = realloc(postdata, strlen(postdata) + strlen(encoded) + 1);
+    if (postdata == NULL) {
+        perror("realloc()");
+        exit(EXIT_FAILURE);
+    }
+    strcat(postdata, encoded);
+    curl_free(encoded);
+    return postdata;
 }
 
 void main(int argc, char **argv)
 {
-    if (argc != 3) {
-        fprintf(stderr, "usage: add_point name float_val\n");
-        exit(EXIT_FAILURE);
+    CURL *handle;
+    char *encoded;
+    char *postdata;
+    char *endptr;
+    double val;
+
+    if (argc != 4) {
+        print_usage();
+        goto error_case;
     }
-    int sock;
-    ssize_t ret;
-    size_t offset;
-    char *encbuf;
-    char *buf;
-    char cl[1000];
 
-    sock = connected_socket("the-tempest.appspot.com", 80);
-    if (sock < 0)
-        goto fail_main;
-
-    buf = malloc(1000);
-    memcpy(buf, "name=", 5);
-    offset = 5;
-
-    ret = urlencode(argv[1], 0, &encbuf);
-    if (ret < 0)
-        goto fail_main;
-    memcpy(buf + offset, encbuf, ret);
-    offset += ret;
-
-    memcpy(buf + offset, "&value=", 7);
-    offset += 7;
-
-    free(encbuf);
-    ret = urlencode(argv[2], 0, &encbuf);
-    if (ret < 0)
-        goto fail_main;
-    memcpy(buf + offset, encbuf, ret);
-    offset += ret;
-    buf[offset] = '\0';
-    free(encbuf);
-
-    if (sendall(sock,
-                "POST /api/create/point HTTP/1.0\r\n"
-                "User-Agent: add-point/1.0\r\n"
-                "Connection: close\r\n"
-                "Content-Type: application/x-www-form-urlencoded\r\n"
-                , 0) < 0) {
-        goto fail_main;
+    /* sanity check the api key */
+    if (strlen(argv[1]) != 32) {
+        print_usage();
+        fprintf(stderr, "\nerror: invalid api key \"%s\"\n", argv[1]);
+        goto error_case;
     }
-    encbuf = malloc(100);
-    if (encbuf < 0) {
-        perror("malloc()");
-        goto fail_main;
+
+    /* sanity check the value we've been given */
+    val = strtod(argv[3], &endptr);
+    if (!val && (argv[3] == endptr)) {
+        print_usage();
+        fprintf(stderr, "\nerror: invalid value \"%s\"\n", argv[3]);
+        goto error_case;
     }
-    sprintf(encbuf, "Content-Length: %zd\r\n\r\n", offset);
 
-    sendall(sock, encbuf, 0);
-    sendall(sock, buf, offset);
+    /* set up curl */
+    curl_global_init(LEXIGRAPH_CURL_OPTS);
+    handle = curl_easy_init();
 
+    /* construct the POST payload */
+    postdata = malloc(1);
+    postdata[0] = '\0';
+    postdata = add_field(handle, postdata, "key=", argv[1]);
+    postdata = add_field(handle, postdata, "dataset=", argv[2]);
+    postdata = add_field(handle, postdata, "value=", argv[3]);
+
+    curl_easy_setopt(handle, CURLOPT_POSTFIELDS, postdata);
+    curl_easy_setopt(handle, CURLOPT_URL, LEXIGRAPH_DATAPOINT_URL);
+    if (curl_easy_perform(handle)) {
+        perror("curl_easy_perform()");
+        goto error_case;
+    }
+    
+    curl_easy_cleanup(handle);
+    curl_global_cleanup();
     exit(EXIT_SUCCESS);
 
-fail_main:
+error_case:
+    curl_easy_cleanup(handle);
+    curl_global_cleanup();
     exit(EXIT_FAILURE);
 }

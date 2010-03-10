f13adc3ed8f42c26c55ccefa1f8768a66968d81f
Evan Klitzke <evan@eklitzke.org>
2010-03-09 23:12:11 -0800

    various bugs

--- lexigraph/view/composite_dataset.py
+++ lexigraph/view/composite_dataset.py
@@ -6,11 +6,22 @@ class NewCompositeDataSet(SessionHandler):
 
     requires_login = True
 
+    def check_allowed(self, datasets):
+        count = 0
+        for ds in model.DataSet.all().filter('account =', self.account).filter('name IN', datasets):
+            count += 1
+            if not (ds.is_allowed(read=True) and ds.is_allowed(write=True)):
+                self.session['error_message'] = 'Insufficient privileges'
+                self.redirect('/dashboard')
+        if count < len(set(datasets)):
+            self.session['error_message'] = 'Failed to find all specified datasets'
+            self.redirect('/dashboard')
+
     def post(self):
         name = self.form_required('name')
         datasets = self.form_required('datasets')
         datasets = [x.strip() for x in datasets.split(',')]
-        self.get_datasets(datasets)
+        self.check_allowed(datasets)
 
         # TODO: validate tags
         tags = self.form_required('tags')
